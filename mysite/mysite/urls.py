122663c558dec8493dfa4c8dad6f37d71041e375
anuragkumart16 <anuragkumartiwari12@gmail.com>
2024-05-10 20:30:11 +0530

    added the home page view

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Pages.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home,name="home"),
 ]
