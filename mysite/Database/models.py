8b31fe41bbdf3f81df2fa6498f96c90a3a74c1db
anuragkumart16 <anuragkumartiwari12@gmail.com>
2024-05-12 08:40:34 +0530

    creation of JossaCollegeData and JossaCollegerRanchData

--- mysite/Database/models.py
+++ mysite/Database/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class JossaCollegeData (models.Model):
+    CollegeID=models.IntegerField()
+    CollegeName = models.CharField(max_length=100)
+    CollegeDescription= models.CharField(max_length=500)
+    OpeningRank=models.CharField(max_length=10)
+    ClosingRank = models.CharField(max_length=10)
+    HeighestPackage=models.CharField(max_length=20)
+    AveragePackage = models.CharField(max_length=20)
+    CampusFile = models.FileField()
+
+
+class JossaCollegeBranchData (models.Model):
+    CollegeID = models.IntegerField()
+    BranchName = models.CharField(max_length=100)
+    BranchOpeningRank = models.CharField(max_length=100)
+    BranchClosingRank = models.CharField(max_length=100)
